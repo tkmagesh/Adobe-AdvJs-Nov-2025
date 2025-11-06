c35145bb8aed9ac2790dbdf80f8ed872294a72c9
Magesh Kuppan <tkmagesh77@gmail.com>
2025-11-06 11:42:36 +0530

    test updates

--- 01-language/__tests__/calculator.test.js
+++ 01-language/__tests__/calculator.test.js
@@ -7,3 +7,55 @@ test("add(100,200) => 300", () => {
     let actual = add(100,200)
     expect(expected).toBe(actual)
 });
+
+
+test('add(100, "200") => 300', ()=> {
+    let expected = 300;
+    let actual = add(100, "200");
+    expect(expected).toBe(actual);
+})
+
+/*
+test('add(100, "abc")', ()=> {
+
+})
+
+test('add(100)', ()=> {
+
+})
+
+test('add()', ()=> {
+
+})
+
+test('add(100, 200, 300, 400, 500)', ()=> {
+
+})
+
+test('add([100, 200])', ()=> {
+
+})
+
+test('add([100, "200"])', ()=> {
+
+})
+
+test('add(100, ["abc"])', ()=> {
+
+})
+
+test('add([100, 200, 300], [400, 500])', ()=> {
+
+})
+
+test('add([[100, 200, 300],[400, 500]])', ()=> {
+
+})
+
+test('add(function(){ return 100;}, function(){ return "200"; })', ()=> {
+
+})
+
+test('add(function(){ return [100, 200, 300]}, function(){ return [400, 500];})', ()=> {
+
+}) */
